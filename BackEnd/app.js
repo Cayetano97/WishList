95d662d4b64088ecad1fe253b9f412bdf95912e3
Cayetano97 <lopezcayetano315@gmail.com>
2023-10-01 14:01:05 +0200

    start backend

---
+++ BackEnd/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+mongoose.set("strictQuery", false);
+mongoose.set("runValidators", true);
+
+const PORT = 8000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const URL = process.env.DATABASE_URL;
+
+mongoose.connect(URL, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.once("Connected", () => {
+  console.log("Database connected");
+});
+
+db.on("Disconected", () => {
+  console.log("Database disconected");
+});
+
+db.on("Error", (error) => {
+  console.log("Database error", error);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
