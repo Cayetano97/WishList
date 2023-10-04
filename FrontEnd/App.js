740b94b280ec764eddc82d9bc314e2d853454988
ClaraMars <clara.marquez.torres@gmail.com>
2023-10-04 18:52:50 +0200

    Fonts added

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,19 +1,67 @@
 import { StatusBar } from "expo-status-bar";
+import globalstyles from "./Globalstyles";
 import { StyleSheet, Text, View } from "react-native";
-import LoginRegister from "./components/Login-Register/Login-Register";
+import * as Font from 'expo-font';
+import { useState, useEffect } from "react";
+// import { useFonts, 
+//   Inter_100Thin,
+//   Inter_200ExtraLight,
+//   Inter_300Light,
+//   Inter_400Regular,
+//   Inter_500Medium,
+//   Inter_600SemiBold,
+//   Inter_700Bold,
+//   Inter_800ExtraBold,
+//   Inter_900Black, } from '@expo-google-fonts/inter';
+// import { AppLoading } from 'expo';
+// import LoginRegister from "./components/Login-Register/Login-Register";
 
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+  const loadFont = async () => {
+    await Font.loadAsync({
+      Inter_100Thin: require('./assets/fonts/Inter/static/Inter-Thin.ttf'),
+      Inter_200ExtraLight: require('./assets/fonts/Inter/static/Inter-ExtraLight.ttf'),
+      Inter_300Light: require('./assets/fonts/Inter/static/Inter-Light.ttf'),
+      Inter_400Regular: require('./assets/fonts/Inter/static/Inter-Regular.ttf'),
+      Inter_500Medium: require('./assets/fonts/Inter/static/Inter-Medium.ttf'),
+      Inter_600SemiBold: require('./assets/fonts/Inter/static/Inter-SemiBold.ttf'),
+      Inter_700Bold: require('./assets/fonts/Inter/static/Inter-Bold.ttf'),
+      Inter_800ExtraBold: require('./assets/fonts/Inter/static/Inter-ExtraBold.ttf'),
+      Inter_900Black: require('./assets/fonts/Inter/static/Inter-Black.ttf'),
+    });
+    setFontLoaded(true);
+  };
+  loadFont();
+  }, []);
+
+  if (!fontLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
+
   return (
-    <View style={styles.container}>
-      <LoginRegister />
-    </View>
+    <>
+    {/* <View style={styles.container}> */}
+      {/* <LoginRegister /> */}
+    {/* </View> */}
+      <StatusBar style="auto" />
+      
+      <View style={styles.container}>
+        <Text style={[globalstyles.input, globalstyles.placeholder]}>Hola</Text>
+      </View>
+      </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    // backgroundColor: "#000",
+    backgroundColor: "#f7f7f7",
     alignItems: "center",
     justifyContent: "center",
   },
---
+++ FrontEnd/Globalstyles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+
+
+const globalstyles = StyleSheet.create({
+
+    input: {
+        backgroundColor: "#fff",
+        width: 350,
+        height: 48,
+        borderStyle: "solid",
+        borderColor: "transparent",
+        borderRadius: 8,
+        shadowColor: "#cecece",  
+        elevation: 13,
+        padding: 13,
+    },
+
+    placeholder: {
+        color: "#636262", 
+        fontSize: 17,
+        fontFamily: "Inter_300Light",
+    },
+
+
+
+});
+
+export default globalstyles;
