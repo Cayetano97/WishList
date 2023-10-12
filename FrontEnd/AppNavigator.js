1d9dd6263f199cdc20957d79cade083081b4bde3
Cayetano97 <lopezcayetano315@gmail.com>
2023-10-12 11:47:00 +0200

    init navbar

--- FrontEnd/AppNavigator.js
+++ FrontEnd/AppNavigator.js
@@ -2,10 +2,13 @@ import { createStackNavigator } from "@react-navigation/stack";
 import LoginRegister from "./components/Login-Register/Login-Register";
 import CustomizeProfile from "./components/Login-Register/Customize-Profile";
 import Home from "./components/Home/Home";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
+  const Tab = createBottomTabNavigator();
+
   return (
     <Stack.Navigator>
       <Stack.Screen
@@ -18,13 +21,17 @@ const AppNavigator = () => {
         component={CustomizeProfile}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{ headerShown: false }}
-      />
+      <Stack.Screen name="Home" options={{ headerShown: false }}>
+        {() => (
+          <Tab.Navigator>
+            <Tab.Screen
+              name="Settings"
+              component={Settings}
+              options={{ headerShown: false }}
+            />
+          </Tab.Navigator>
+        )}
+      </Stack.Screen>
     </Stack.Navigator>
   );
 };
-
-export default AppNavigator;
