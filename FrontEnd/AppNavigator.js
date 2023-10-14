ddf4e6c267634fa351f14020cfab312a38258948
Cayetano97 <lopezcayetano315@gmail.com>
2023-10-14 11:50:17 +0200

    navbar ok

--- FrontEnd/AppNavigator.js
+++ FrontEnd/AppNavigator.js
@@ -1,5 +1,12 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View, Image } from "react-native";
+
+import IconLists from "./assets/img/navbar/Lists.png";
+import IconCollections from "./assets/img/navbar/Collections.png";
+import IconHome from "./assets/img/navbar/Home.png";
+import IconStats from "./assets/img/navbar/Stats.png";
+import IconFriends from "./assets/img/navbar/Friends.png";
 
 import LoginRegister from "./components/Login-Register/Login-Register";
 import CustomizeProfile from "./components/Login-Register/Customize-Profile";
@@ -11,19 +18,138 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator
-      screenOptions={{ tabBarStyle: { backgroundColor: "#F7F7F7" } }}
+      screenOptions={{
+        tabBarStyle: { backgroundColor: "#F7F7F7" },
+        tabBarActiveTintColor: "#000000",
+        tabBarInactiveTintColor: "#A9A9A9",
+        tabBarLabelStyle: { fontSize: 13 },
+        tabBarStyle: { height: 55 },
+        headerShown: false,
+      }}
     >
-      <Tab.Screen name="Listas" component={Test} />
-      <Tab.Screen name="Colecciones" component={Test} />
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Estadísticas" component={Test} />
-      <Tab.Screen name="Amigos" component={Test} />
+      <Tab.Screen
+        name="Listas"
+        component={Test}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{ borderTopWidth: focused ? 2 : 0, borderColor: "black" }}
+            >
+              <Image
+                source={IconLists}
+                style={{
+                  width: 60,
+                  height: "100%",
+                  tintColor: focused ? "black" : "grey",
+                }}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Colecciones"
+        component={Test}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                borderTopWidth: 2,
+                borderColor: focused ? "black" : "transparent",
+              }}
+            >
+              <Image
+                source={IconCollections}
+                style={{
+                  width: 60,
+                  height: "100%",
+                  tintColor: focused ? "black" : "grey",
+                }}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={Test}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                borderTopWidth: 2,
+                borderColor: focused ? "black" : "transparent",
+              }}
+            >
+              <Image
+                source={IconHome}
+                style={{
+                  width: 60,
+                  height: "100%",
+                  tintColor: focused ? "black" : "grey",
+                }}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Estadísticas"
+        component={Test}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                borderTopWidth: 2,
+                borderColor: focused ? "black" : "transparent",
+              }}
+            >
+              <Image
+                source={IconStats}
+                style={{
+                  width: 60,
+                  height: "100%",
+                  tintColor: focused ? "black" : "grey",
+                }}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Amigos"
+        component={Test}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                borderTopWidth: 2,
+                borderColor: focused ? "black" : "transparent",
+              }}
+            >
+              <Image
+                source={IconFriends}
+                style={{
+                  width: 60,
+                  height: "100%",
+                  tintColor: focused ? "black" : "grey",
+                }}
+              />
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
 const AppNavigator = () => {
   return (
     <Stack.Navigator>
+      <Stack.Screen
+        name="Navbar"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen
         name="Login"
         component={LoginRegister}
@@ -32,11 +158,6 @@ const AppNavigator = () => {
       <Stack.Screen
         name="CustomizeProfile"
         component={CustomizeProfile}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Navbar"
-        component={TabNavigator}
         options={{ headerShown: false }}
       />
       <Stack.Screen
